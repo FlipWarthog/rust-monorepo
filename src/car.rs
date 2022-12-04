50b1e3fbecefd8fc435a95849b1270b03c00e526
FlipWarthog <flipwarthog@gmail.com>
2022-12-03 21:37:04 -0500

    Fix SQL, add car module

---
+++ src/car.rs
@@ -0,0 +1,78 @@
+use bigdecimal::BigDecimal;
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+
+use crate::schema::{car, car::dsl::*};
+use diesel::result::Error;
+
+#[derive(Debug, PartialEq, Eq, Queryable)]
+pub struct Car {
+    pub id: i32,
+    pub vin: String,
+    pub make: String,
+    pub model: String,
+    pub year: i32,
+    pub color: String,
+    pub price: BigDecimal,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = car)]
+pub struct NewCar<'a> {
+    pub vin: &'a str,
+    pub make: &'a str,
+    pub model: &'a str,
+    pub year: i32,
+    pub color: &'a str,
+    pub price: BigDecimal,
+}
+
+pub struct CarResource<'a> {
+    conn: &'a mut PgConnection,
+}
+
+impl<'a> CarResource<'a> {
+    pub fn with(conn: &'a mut PgConnection) -> Self {
+        Self { conn }
+    }
+
+    pub fn get_single(&mut self, i: i32) -> Result<Car, Error> {
+        car.find(i).first(self.conn)
+    }
+
+    pub fn get_manufacturers(&mut self) -> Result<Vec<String>, Error> {
+        car.select(make)
+            .distinct()
+            .order(make.asc())
+            .load::<String>(self.conn)
+    }
+
+    pub fn create(&mut self, new_car: NewCar) -> Result<Car, Error> {
+        diesel::insert_into(car::table)
+            .values(&new_car)
+            .get_result(self.conn)
+    }
+
+    pub fn update(&mut self, update_car: Car) -> Result<Car, Error> {
+        diesel::update(car.find(update_car.id))
+            .set((
+                vin.eq(update_car.vin),
+                make.eq(update_car.make),
+                model.eq(update_car.model),
+                year.eq(update_car.year),
+                color.eq(update_car.color),
+                price.eq(update_car.price),
+            ))
+            .get_result::<Car>(self.conn)
+    }
+
+    pub fn delete(&mut self, i: i32) -> Result<usize, Error> {
+        diesel::delete(car.find(i)).execute(self.conn)
+    }
+
+    // TODO: Add filters and sorts
+    pub fn list(&mut self) -> Result<Vec<Car>, Error> {
+        car.order(make.asc()).load::<Car>(self.conn)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,45 @@
+#![allow(dead_code, unused)]
+
+mod car;
 mod schema;
 use std::env;
 
-use bigdecimal::BigDecimal;
-use chrono::NaiveDateTime;
+use bigdecimal::{BigDecimal, FromPrimitive};
+use car::CarResource;
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use schema::car::dsl::*;
 
-#[derive(Debug, PartialEq, Eq, Queryable)]
-pub struct Car {
-    id: i32,
-    vin: String,
-    make: String,
-    model: String,
-    year: i32,
-    color: String,
-    price: BigDecimal,
-    updated_at: NaiveDateTime,
-}
+use crate::car::NewCar;
 
 fn main() {
     let mut conn = establish_connection();
+    let mut car_resource = CarResource::with(&mut conn);
 
-    let cars = car.load::<Car>(&mut conn).expect("Error connecting?");
+    let r = car_resource.create(NewCar {
+        vin: "new val",
+        make: "new val",
+        model: "new val",
+        year: 2022,
+        color: "new val",
+        price: BigDecimal::from_f32(2000.00).unwrap(),
+    });
+    if let Ok(c) = r {
+        println!("Car added! {:?}", c);
 
-    for c in cars {
-        println!("{:?}", c);
+        let mut update_car = c;
+
+        update_car.make.push_str(", update");
+
+        let r = car_resource.update(update_car);
+
+        if let Ok(u) = r {
+            println!("Update car is {:?}", u);
+
+            let d = car_resource.delete(u.id);
+            if let Ok(n) = d {
+                println!("Deleted car. Response is {n}");
+            }
+        }
     }
 }
 
